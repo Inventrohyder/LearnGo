a441ebe47d99f6032c3caa94c8510a3dc5c0322d
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-03-10 10:58:09 +0300

    ✅ exercise: complete weather forecast commenting

--- exercism/go/weather-forecast/weather_forecast.go
+++ exercism/go/weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather provides weather forecast information.
 package weather
 
+// CurrentCondition stores the current weather condition.
 var CurrentCondition string
+// CurrentLocation stores the current location to be forecasted.
 var CurrentLocation string
 
+// Forecast provides the weather forecast of a certain location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
