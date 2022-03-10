d985835a4bc5ed0e55b799cd20a5403568f1abdc
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-03-10 13:16:48 +0300

    ✅ exercise: solve slices

--- go.dev/Tour/Exercises/Exercise Slices/exercise-slices.go
+++ go.dev/Tour/Exercises/Exercise Slices/exercise-slices.go
@@ -3,6 +3,15 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	image := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		var imageRow = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			imageRow[j] = uint8(i*j)
+		}
+		image[i] = imageRow
+	}
+	return image
 }
 
 func main() {
