654cc0dc82dd9f84db21a0a5a6a51dccd97b5dc3
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-03-15 12:50:49 +0300

    ✅ exercise: solve equivalent binary trees

--- go.dev/Tour/Exercises/Exercise Equivalent Binary Trees/exercise-equivalent-binary-trees.go
+++ go.dev/Tour/Exercises/Exercise Equivalent Binary Trees/exercise-equivalent-binary-trees.go
@@ -1,14 +1,59 @@
 package main
 
-import "golang.org/x/tour/tree"
+import (
+	"fmt"
+	"golang.org/x/tour/tree"
+)
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func Walk(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		Walk(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		Walk(t.Right, ch)
+	}
+}
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
+
+	for {
+		n1, ok1 := <-ch1
+		n2, ok2 := <-ch2
+
+		// if only a single channel is open
+		// or the values are different,
+		// then we have different trees
+		if ok1 != ok2 || n1 != n2 {
+			return false
+		}
+
+		// If we reach here then it means both tree
+		// channels are closed
+		if !ok1 {
+			break
+		}
+	}
+
+	return true
+}
 
 func main() {
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
